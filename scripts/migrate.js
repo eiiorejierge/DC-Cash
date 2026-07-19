a6bf18b0384d9cb318c61a48a7409f3a4b705029
eiiorejierge <will63140@gmail.com>
2026-07-19 01:07:21 -0600

    Add scripts/migrate.js

---
+++ scripts/migrate.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const { migrate, pool } = require('../db');
+
+migrate()
+  .then(() => console.log('Database migration complete.'))
+  .then(() => pool.end())
+  .catch(error => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+
